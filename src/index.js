52ebf1b091444bfc88154bac6ef8a55520bfc807
Filipp Gudis <fgudis@yahoo.com>
2020-09-21 22:03:33 -0700

    Interceptors in USE!!!!

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import axios from "axios";
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+axios.interceptors.request.use(
+  requestconfig => {
+    console.log(requestconfig);
+    // idea behing interceptors - we can edit and
+    //alter our request config before returning it!!
+    return requestconfig;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+axios.interceptors.response.use(
+  response => {
+    console.log(response);
+    // idea behing interceptors - we can edit and
+    //alter our request config before returning it!!
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
