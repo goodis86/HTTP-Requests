4777b15f5b6518ee664bfd5ad034af6d4528d79a
Filipp Gudis <fgudis@yahoo.com>
2020-09-21 21:52:29 -0700

    ERROR HANDLING in http requests

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -11,7 +11,8 @@ class Blog extends Component {
 
     state = {
         posts:[],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
   componentDidMount() {
@@ -26,6 +27,10 @@ class Blog extends Component {
         })
         this.setState({posts: updatedPosts});
         //console.log(response);
+    })
+    .catch(error => {
+       // console.log(error);
+        this.setState({error: true})
     });
   }
 
@@ -35,15 +40,20 @@ class Blog extends Component {
 
   render() {
 
-    const posts = this.state.posts.map(post => {
-        return <Post 
-        key = {post.id} 
-        title = {post.title}
-        author = {post.author}
-        clicked = {() => this.postClickedHandler(post.id)}/>
-    }
+    let posts = <p style = {{textAlign: 'center'}}>Something went wrong!!</p>
 
-    );
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post 
+                key = {post.id} 
+                title = {post.title}
+                author = {post.author}
+                clicked = {() => this.postClickedHandler(post.id)}/>
+            }
+        
+            );
+        }
+  
     return (
       <div>
         <section className="Posts">
